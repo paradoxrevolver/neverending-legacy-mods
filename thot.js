15a46f2d1099c28a2a02cb15ed9b8db53d1a374e
rubic <adamtheball@gmail.com>
2017-03-25 21:07:18 -0700

    Add files via upload

---
+++ thot.js
@@ -0,0 +1,27 @@
+G.AddData({
+    Name:'Thot Mod',
+    Author:'fancy and torcado',
+    desc:'This mod makes it possible to create thots. Thots are an improved version of Dreamers.',
+    engineVersion:1,
+    manifest:'modManifest.js',
+    requires:['Default dataset*'],
+    sheets:{'thotSheet':'img/thotModIconSheet.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
+    func:function()
+    {
+		new G.Unit({
+		name:'thot',
+		desc:'@generates [insight] more frequently than a [dreamer]<>A [thot] spends their time observing, thinking, and wondering why things are the way they are.',
+		icon:[0,0,'thotSheet'],
+		cost:{'food':50},
+		use:{'worker':1},
+		effects:[
+			{type:'gather',what:{'insight':0.3}},
+			{type:'gather',what:{'insight':0.15},req:{'symbolism':true}},
+			{type:'mult',value:1.2,req:{'wisdom rituals':'on'}}
+		],
+		category:'discovery',
+		req:{'construction':true},
+		limitPer:{'house':1},
+		});
+    }
+});
